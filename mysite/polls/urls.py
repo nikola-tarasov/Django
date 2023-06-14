3df89133ea91c64855a356628161907934868826
nikola-tarasov <nikola-tarasov@mail.ru>
2023-06-14 12:06:50 +0300

    Fifth commit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # added the word 'specifics'
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -9,3 +10,6 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
